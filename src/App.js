de61108d2a622297d0f01e1a020844ed6ec086fb
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2024-04-30 07:04:16 +0300

    Add to cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { MainButton } from '@vkruglikov/react-telegram-web-app';
 import './App.css';
 import Product from './components/Product';
+import { useShoppingCart } from './context/useShoppingCart';
 import products from './data/index'
 function App() {
-  console.log(products)
+  const { cart } = useShoppingCart()
   return (
     <div class="products flex" style={{width:'100%', maxWidth:'768px', margin:'0 auto',paddingTop:'20px'}}>
        {products.map((data,i)=>(
          <Product key={i} product = {data} />
        ))}
+       {cart?.length > 0 && 
+         <MainButton text="VIEW ORDER" onClick={()=>{alert("order page")}}/>
+       }
     </div>
   );
 }
