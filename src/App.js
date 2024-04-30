5f5da7869771be19867b16da5a9ba38e26fd3f43
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2024-04-30 07:20:18 +0300

    Add to cart

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 import Product from './components/Product';
 import { useShoppingCart } from './context/useShoppingCart';
 import products from './data/index'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+
 function App() {
-  const { cart } = useShoppingCart()
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Products/>,
+    },
+    {
+      path: "cart",
+      element: <Cart/>,
+    },
+  ]);
+ 
   return (
-    <div class="products flex" style={{width:'100%', maxWidth:'768px', margin:'0 auto',paddingTop:'20px'}}>
-       {products.map((data,i)=>(
-         <Product key={i} product = {data} />
-       ))}
-       {cart?.length > 0 && 
-         <MainButton text="VIEW ORDER" onClick={()=>{alert("order page")}}/>
-       }
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/Products.jsx
@@ -0,0 +1,24 @@
+import { MainButton } from '@vkruglikov/react-telegram-web-app';
+
+import Product from '../components/Product';
+import { useShoppingCart } from '../context/useShoppingCart';
+import products from '../data/index'
+import { useNavigate } from 'react-router-dom';
+
+const Products = ()=>{
+    const { cart } = useShoppingCart()
+    const navigate = useNavigate()
+  return (
+    <div class="products flex" style={{width:'100%', maxWidth:'768px', margin:'0 auto',paddingTop:'20px'}}>
+    {products.map((data,i)=>(
+      <Product key={i} product = {data} />
+    ))}
+    {cart?.length > 0 && 
+      <MainButton text="VIEW ORDER" onClick={()=>{navigate('/cart')}}/>
+    }
+ </div>
+  )
+}
+
+
+export default Products
