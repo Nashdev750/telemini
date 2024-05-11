41ce13271faa12a3fffc61e7ca08859d70507cfb
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2024-05-11 16:42:55 +0300

    init data

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import {
 } from "react-router-dom";
 import Products from './pages/Products';
 import Cart from './pages/Cart';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Checkout from './pages/Checkout';
 import ThankYou from "./pages/ThankYou";
 
 function App() {
+  const [initdata, setInitdata] = useState("")
 
 useEffect(()=>{
   if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.enableClosingConfirmation) {
       window.Telegram.WebApp.enableClosingConfirmation();
+      console.log(window.Telegram.WebApp)
+      setInitdata(JSON.stringify(window.Telegram.WebApp))
   }
 },[])
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Products/>,
+      element: <><h5 style={{width:"300px"}}>{initdata}</h5><Products/></>,
     },
     {
       path: "/cart",
